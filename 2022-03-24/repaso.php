e3c16e40bf0e529d5091aaa39242be0bfbe11aca
estevantorres <102324146+estevantorres@users.noreply.github.com>
2022-03-24 17:29:41 -0500

    Add files via upload

---
+++ 2022-03-24/repaso.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+</head>
+<body>
+ <h1>Hola soy un titulo</h1>
+ <?php
+ // INICIA CODIGO php
+ //Esto es un comentario
+ /* todo lo que este aqui dentro
+ se considera un comentario
+ */
+ // COMANDO ECHO DEVUELVE UN VALOR
+ echo "HOLA SOY UN PARRAFO";
+ echo "<br>";  //salto de linea
+ echo 10+6;  /*realiza operaciones
+              que no se puede en html.*/
+ // TERMINA php
+ ?>
+
+ <?php
+// definimos una variable con un texto
+$miVariable = "HOLA VARIABLE";
+//visualizamos la variable en el explorador
+echo $miVariable;
+//otro ejemplo variable miValor igual a 2
+echo "<br>";
+$mivalor = 5;
+//visualizamos sin embargo realizamos una multiplicacion * 3 
+echo 3*$mivalor;
+ ?>
+ </BODY>
+ </html>
---
+++ 2022-03-24/variables.php
@@ -0,0 +1,11 @@
+
+<?php
+// definimos una variable con un texto
+$miVariable = "HOLA VARIABLE";
+//visualizamos la variable en el explorador
+echo $miVariable;
+//otro ejemplo variable miValor igual a 2
+echo "<br>";
+$mivalor = 5;
+//visualizamos sin embargo realizamos una multiplicacion * 3 
+echo 3*$mivalor;
