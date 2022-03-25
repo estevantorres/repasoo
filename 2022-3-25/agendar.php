c7e7bb440b322ef013ebbc091fb78dd3b011477d
estevantorres <102324146+estevantorres@users.noreply.github.com>
2022-03-25 16:54:00 -0500

    Add files via upload

---
+++ 2022-3-25/agendar.php
@@ -0,0 +1,11 @@
+<?php
+// creamos una matriz unidimensional llamada frutas
+$fruta[0] = "manzana";
+$fruta[1] = "pera";
+$fruta[2] = "fresa";
+
+// realizamos un for para escribir los valores de la matriz
+for ($numero=0;$numero<=2;$numero++) {
+echo $fruta[$numero]."<br>";
+}
+?>
